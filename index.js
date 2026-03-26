da78c4d5c8150586d564e94742398e6d076d47e2
Akany <essienakany@gmail.com>
2026-03-26 13:00:35 +0100

    updated

---
+++ index.js
@@ -0,0 +1,87 @@
+// let player = {
+//     name: "Akany",
+//     chips: 600
+// }
+
+// let cards = []
+// let sum = 0
+// let hasBlacJack = false
+// let isAlive = false
+// let message = ""
+// let messageEl = document.getElementById("message-el")
+// let sumEl = document.getElementById("sum-el")
+// let cardsEl = document.getElementById("cards-el")
+
+// let playerEl = document.getElementById("player-el")
+
+// playerEl.textContent = player.name + ": $" + player.chips
+
+// function getRandomCard(){
+//     let randomNumber = Math.floor( Math.random() * 13 ) + 1
+
+//     if (randomNumber === 1){
+//         return 11
+//     } else if (randomNumber > 10){
+//         return 10
+//     } else {
+//         return randomNumber
+//     }
+// }
+
+// function startGame(){
+//     let firstCard = getRandomCard()
+//     let secondCard = getRandomCard()
+
+//     cards = [firstCard, secondCard]
+//     sum = firstCard + secondCard
+
+//     isAlive = true
+//     renderGame()
+// }
+
+
+// function renderGame(){
+
+//     sumEl.textContent = "Sum: "  + sum
+//     cardsEl.textContent = "Cards: " 
+
+//     for (let i = 0; i < cards.length; i++){
+//         cardsEl.textContent += cards[i] + " "
+//     }
+
+//     if (sum <= 20){
+//         message = "Do you want to draw a new care?"
+//     } else if (sum === 21){
+//         message = "You've got Blackjack!"
+//         hasBlacJack = true    
+//     } else {
+//         message = "You're out of the game"
+//         isAlive = false
+//     }
+
+//     messageEl.textContent = message
+// }
+
+
+// function newCard(){
+
+//     if (isAlive === true && hasBlacJack === false){
+
+//         let drawCard = getRandomCard()
+//         sum += drawCard;
+//         cards.push(drawCard)
+
+//         renderGame()
+//     }
+// }
+
+
+let hands = ["rock", "paper", "scissor"]
+
+function getHand(){
+    
+    let theHand = Math.floor(Math.random() * hands.length)
+    console.log(hands[theHand])
+}
+
+getHand()
